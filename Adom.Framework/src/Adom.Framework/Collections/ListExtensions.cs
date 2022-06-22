a20fffdaeb73bc43a7111d3f313fbe8e8a5b04e7
wcontayon <cwcontayon@talentsoft.com>
2022-06-22 11:35:54 +0200

    Add extension (method Add) to ListExtensions

--- Adom.Framework/src/Adom.Framework/Collections/ListExtensions.cs
+++ Adom.Framework/src/Adom.Framework/Collections/ListExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Adom.Framework.Collections
@@ -12,6 +13,45 @@ namespace Adom.Framework.Collections
         /// <param name="list">The list</param>
         /// <param name="items">Items to be added</param>
         public static void Add<T>(this IList<T> list, params T[] items)
+        {
+            Debug.Assert(list != null);
+
+            for (int i = 0; i < items.Length; i++) list.Add(items[i]);
+        }
+
+        /// <summary>
+        /// Add items into the list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">The list</param>
+        /// <param name="items">Items to be added</param>
+        public static void Add<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            Debug.Assert(list != null);
+
+            foreach (var item in items) list.Add(item);
+        }
+
+        /// <summary>
+        /// Add items into the list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">The list</param>
+        /// <param name="items">Items to be added</param>
+        public static void Add<T>(this IList<T> list, ReadOnlySpan<T> items)
+        {
+            Debug.Assert(list != null);
+
+            for (int i = 0; i < items.Length; i++) list.Add(items[i]);
+        }
+
+        /// <summary>
+        /// Add items into the list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">The list</param>
+        /// <param name="items">Items to be added</param>
+        public static void Add<T>(this IList<T> list, Span<T> items)
         {
             Debug.Assert(list != null);
 
