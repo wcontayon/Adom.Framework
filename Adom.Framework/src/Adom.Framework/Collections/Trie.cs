480e15effdbfece34e179124ac0cfe4818b7e1fc
wcontayon <williamcontayon@outlook.com>
2023-05-31 16:36:34 +0200

    add trie data structure

--- Adom.Framework/src/Adom.Framework/Collections/Trie.cs
+++ Adom.Framework/src/Adom.Framework/Collections/Trie.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("Adom.Framework.Tests")]
 
 namespace Adom.Framework.Collections
 {
@@ -14,6 +17,7 @@ namespace Adom.Framework.Collections
         private TrieNodeCollection _childrens;
         private TrieNode? _parent;
         private int _level;
+        private string _word = string.Empty;
 
         public TrieNode(char val, int level, TrieNode? parent)
         {
@@ -21,6 +25,7 @@ namespace Adom.Framework.Collections
             _level = level;
             _parent = parent;
             _childrens = new TrieNodeCollection();
+            // _word = wordPrefix;
         }
 
         public bool IsLeaf { get => _childrens == null || _childrens.Count == 0; }
@@ -29,13 +34,17 @@ namespace Adom.Framework.Collections
 
         public TrieNode? Parent { get => _parent; }
 
+        public TrieNodeCollection Children { get => _childrens; }
+
         public int Level { get => _level; }
+
+        public string Word { get => _word; }
 
         internal TrieNode? FindChar(char c)
         {
             Debug.Assert(_childrens != null);
 
-            for (int i = 0; i< _childrens.Count; i++)
+            for (int i = 0; i < _childrens.Count; i++)
             {
                 if (_childrens[i].Value == c)
                 {
@@ -45,11 +54,24 @@ namespace Adom.Framework.Collections
 
             return null;
         }
+
+        internal void DeleteChar(char c)
+        {
+            Debug.Assert(_childrens != null);
+            for (var i = 0; i < _childrens.Count; i++)
+            {
+                if (_childrens[i].Value == c)
+                {
+                    _childrens.RemoveAt(i);
+                }
+            }
+        }
+
     }
 
     internal class TrieNodeCollection : IEnumerable<TrieNode>
     {
-        private List<TrieNode> _nodes;
+        internal List<TrieNode> _nodes;
 
         public TrieNodeCollection() => _nodes = new List<TrieNode>();
 
@@ -63,6 +85,8 @@ namespace Adom.Framework.Collections
 
         public void Clear() => _nodes.Clear();
 
+        public void RemoveAt(int index) => _nodes.RemoveAt(index);
+
         public IEnumerator<TrieNode> GetEnumerator() => _nodes.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => _nodes.GetEnumerator();
@@ -73,13 +97,21 @@ namespace Adom.Framework.Collections
         public readonly bool WordFound { get; }
 
         public readonly ReadOnlySpan<string> MatchedWords { get; }
+
+        public SearchTrieResult(bool found, string[] matches)
+        {
+            WordFound = found;
+            MatchedWords = matches;
+        }
     }
 
     public class Trie
     {
         private const char ROOTCHAR = '^';
+        private const char ENDCHAR = '$';
 
         private readonly TrieNode? _root;
+        internal TrieNode Root { get => _root; }
 
         public Trie()
         {
@@ -98,10 +130,7 @@ namespace Adom.Framework.Collections
             return result;
         }
 
-        public void InserWord(string word)
-        {
-            
-        }
+        public void InserWord(string word) => Insert(word.AsSpan());
 
         private void Insert(ReadOnlySpan<char> word)
         {
@@ -111,12 +140,44 @@ namespace Adom.Framework.Collections
                 for (int i = currentNode.Level; i < word.Length; i++)
                 {
                     var node = new TrieNode(word[i], currentNode.Level + 1, currentNode);
-                    // currentNode.
+                    currentNode.Children.Add(node);
+                    currentNode = node;
                 }
+
+                currentNode.Children.Add(new TrieNode(ENDCHAR, currentNode.Level + 1, currentNode));
             }
         }
 
-        private TrieNode? PrefixWord(TrieNode? startNode, ReadOnlySpan<char> word)
+        public SearchTrieResult FindWord(string word)
+        {
+            string prefixFound = string.Empty;
+            var node = Prefix(word.AsSpan());
+
+            if (node != null)
+            {
+                var matchedWords = new List<string>();
+
+                // Add the word found
+                matchedWords.Add(word);
+
+                foreach (var child in node.Children)
+                {
+                    var matched = BuildPrefixWord(child);
+                    if (matched == string.Empty)
+                    {
+                        matchedWords.Add(matched);
+                    }
+                }
+
+                return new SearchTrieResult(true, matchedWords.ToArray());
+            }
+            else
+            {
+                return new SearchTrieResult(false, Array.Empty<string>());
+            }
+        }
+
+        internal TrieNode? PrefixWord(TrieNode? startNode, ReadOnlySpan<char> word)
         {
             Debug.Assert(startNode != null);
 
@@ -130,6 +191,21 @@ namespace Adom.Framework.Collections
             }
 
             return startNode;
+        }
+
+        internal string BuildPrefixWord(TrieNode node)
+        {
+            string word = string.Empty;
+            ValueStringBuilder builder = new ValueStringBuilder();
+            builder.Append(node.Value);
+            var i = 0;
+            while (node.Children[i].Value != ENDCHAR)
+            {
+                builder.Append(node.Children[i].Value);
+            }
+
+            word = builder.ToString();
+            return word;
         }
     }
 }
